1df28b3efcccee96781b3148b2274fc10b1e8c17
Paula <pquilodran2017@alu.uct.cl>
2020-10-21 19:29:09 -0300

    arreglos del script de python

--- insertardatos.py
+++ insertardatos.py
@@ -1,5 +1,5 @@
 import mysql.connector
-
+#conectarse a la base de datos
 dbConnect= {
     'host': 'localhost',
     'user':'root',
@@ -10,4 +10,8 @@ conexion = mysql.connector.connect(**dbConnect)
 
 cursor = conexion.cursor()
 #Insertar datos en la tabla regiones
-sqlInsertar= 'insert into regiones ' 
+sqlInsertar= "insert into regiones(ID_Region,Nombre_Reg,N_Hab_Reg,N_Contagiados_Reg,N_Recuperados_Reg,Fecha_Reg)values(%s,%s,%s,%s,%s,%s)" 
+
+cursor.execute(sqlInsertar,('1','Aracunia','2','3','3','12'))
+conexion.commit()
+cursor.close()
