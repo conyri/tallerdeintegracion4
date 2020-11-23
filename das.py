f81480cd17d305cb421fe0e54bd50ca70fec25ce
Paula <pquilodran2017@alu.uct.cl>
2020-11-22 23:44:46 -0300

    cambios

--- das.py
+++ das.py
@@ -4,13 +4,30 @@ import numpy as np
 import pydeck as pdk 
 import altair as alt 
 from datetime import datetime
-DATA_URL = ('covid.csv')
-@st.cache
-def load_data():
-    data = pd.read_csv(DATA_URL)
-    data['Date'] = pd.to_datetime(data['Date']).dt.strftime('%Y-%m-%d')
-    return data
-df = load_data()
 
-# show data on streamlit
- st.write(df)
+from pymongo import MongoClient
+import pymongo
+import time as ti
+
+myclient = pymongo.MongoClient("mongodb+srv://constanza:constanza@cluster0.getl7.mongodb.net/integracion4?retryWrites=true&w=majority")
+
+mydb = myclient["integracion4"]
+mycol = mydb["regiones"]
+
+mydoc = mycol.find()
+
+
+ #select box
+metrics =['Atacama','','total_deaths','new_deaths','total_cases_per_million','new_cases_per_million','total_deaths_per_million','new_deaths_per_million','total_tests','new_tests','total_tests_per_thousand','new_tests_per_thousand']
+cols = st.selectbox('Covid metric to view', metrics)
+
+def get_table():
+    datatable = df[['state', 'confirmed', 'recovered', 'deaths','active']].sort_values(by=['confirmed'], ascending=False)
+    datatable = datatable[datatable['state'] != 'State Unassigned']
+    return datatable
+
+datatable = get_table()
+st.markdown("### Covid-19 cases in India")
+st.markdown("The following table gives you a real-time analysis of the confirmed, active, recovered and deceased cases of Covid-19 pertaining to each state in India.")
+st.dataframe(datatable) # will display the dataframe
+st.table(datatable)# will display the table
