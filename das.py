65b2db3caa8e1d4b5c1bcf73a14838c95cc60240
Paula <pquilodran2017@alu.uct.cl>
2020-11-24 22:18:31 -0300

    cambios 2

--- das.py
+++ das.py
@@ -20,14 +20,14 @@ mydoc = mycol.find()
  #select box
 metrics =['Atacama','','total_deaths','new_deaths','total_cases_per_million','new_cases_per_million','total_deaths_per_million','new_deaths_per_million','total_tests','new_tests','total_tests_per_thousand','new_tests_per_thousand']
 cols = st.selectbox('Covid metric to view', metrics)
+#Esto espara hacer una tabla 
+#def get_table():
+#    datatable = df[['state', 'confirmed', 'recovered', 'deaths','active']].sort_values(by=['confirmed'], ascending=False)
+#    datatable = datatable[datatable['state'] != 'State Unassigned']
+#    return datatable
 
-def get_table():
-    datatable = df[['state', 'confirmed', 'recovered', 'deaths','active']].sort_values(by=['confirmed'], ascending=False)
-    datatable = datatable[datatable['state'] != 'State Unassigned']
-    return datatable
-
-datatable = get_table()
-st.markdown("### Covid-19 cases in India")
-st.markdown("The following table gives you a real-time analysis of the confirmed, active, recovered and deceased cases of Covid-19 pertaining to each state in India.")
-st.dataframe(datatable) # will display the dataframe
-st.table(datatable)# will display the table
+#datatable = get_table()
+#st.markdown("### Covid-19 cases in India")
+#st.markdown("The following table gives you a real-time analysis of the confirmed, active, recovered and deceased cases of Covid-19 pertaining to each state in India.")
+#st.dataframe(datatable) # will display the dataframe
+#st.table(datatable)# will display the table
