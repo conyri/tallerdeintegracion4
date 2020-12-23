de19ff4107d6e904feb6b34da03e9e7fb6573b93
Unknown <rbecerra2017@alu.uct.cl>
2020-12-23 10:14:43 -0300

    Añadido predicción

--- grafica.py
+++ grafica.py
@@ -4,7 +4,10 @@ import time as ti
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error
 
 myclient = pymongo.MongoClient("mongodb+srv://constanza:constanza@cluster0.getl7.mongodb.net/integracion4?retryWrites=true&w=majority")
 
@@ -23,7 +26,28 @@ def region(nombre):
     y.append(int(float(i["N_cont_region"])))
     cont = cont + 1
     x.append(cont)
+  x1 = np.array(x).reshape(-1, 1)
+  y1 = np.array(y).reshape(-1, 1)
+  x_train, x_test, y_train, y_test = train_test_split(x1, y1,
+                                                      test_size=0.2,
+                                                      train_size=0.8,
+                                                      shuffle=True) 
+  linreg = LinearRegression()
+  linreg.fit(x_train,y_train)
+  ypredict = linreg.predict(x_test)
 
+  # Error Cuadrado Medio
+  rmse = np.sqrt(mean_squared_error(ypredict, y_test))
+  print ('Error cuadrado medio: \n',rmse)
+
+  aPred = []
+  aX = []
+  for i in range(0,355):
+    pred = linreg.predict([[i]])
+    aPred.append(int(pred))
+    aX.append(i)
+  
+  plt.plot(aX,aPred,color="red")
   plt.scatter(x,y)
   plt.title(nombre)
   plt.xlabel('Dias')
@@ -32,18 +56,6 @@ def region(nombre):
 
 nombre = str(input("Ingrese nombre de Region: "))
 region(nombre)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
